10e706bba52032455fb11d94578f4c2b44497d8f
cnsheafe <cnsheafe@gmail.com>
2017-08-16 11:50:22 -0600

    Added data annotations for input validation

--- Model/Schemas/User.cs
+++ Model/Schemas/User.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StockWatcher.Model.Schemas {
     public class User {
+        [Required]
+        [StringLength(30)]
         public string Username {get; set;}
+
+        [Required]
+        [StringLength(30)]
         public string Password {get; set;}
+
+        [Required]
+        [StringLength(12)]
         public string Phone {get; set;}
         public string Email {get; set;}
         public string Uuid {get; set;}
