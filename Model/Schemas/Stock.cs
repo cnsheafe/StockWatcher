76389e9a876dd972da5105df0a79fdc08f920a16
cnsheafe <cnsheafe@gmail.com>
2017-08-16 16:38:45 -0600

    Added data annotations

--- Model/Schemas/Stock.cs
+++ Model/Schemas/Stock.cs
@@ -1,19 +1,28 @@
 // Contains Model Schema for Stock
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace StockWatcher.Model.Schemas {
    public class Stock {
-       public string Username {get; set;}
-       public string Equity {get; set;}
 
-       private double price;
-       public double Price {
-           get => Math.Round(price, 2, MidpointRounding.ToEven);
-           set => price = value;
-       } 
-       public string RequestId {
-           get => Equity + price.ToString();
-       }
+    [Required]
+    [StringLength(30)]
+    public string Username {get; set;}
+
+    [Required]
+    [StringLength(4)]
+    public string Equity {get; set;}
+    
+    private double price;
+
+    [Required]
+    public double Price {
+        get => Math.Round(price, 2, MidpointRounding.ToEven);
+        set => price = value;
+    } 
+    public string RequestId {
+        get => Equity + price.ToString();
+    }
 
    } 
 }
