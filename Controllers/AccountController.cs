4119d03521368ef862d5a59ff6b80bc6e0350145
cnsheafe <cnsheafe@gmail.com>
2017-08-20 14:22:47 -0600

    Changed CreateAccount endpoint

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -14,28 +14,43 @@ using Twilio.Rest.Api.V2010.Account;
 using Twilio.Rest.Notify.V1.Service;
 using Twilio.Types;
 
-using StockWatcher.Model.Data;
+using StockWatcher.Model;
 using StockWatcher.Model.Schemas;
-
-using StockWatcher.Model.Actions;
+using StockWatcher.Model.Services;
 
 namespace StockWatcher.Controllers {
-    public class AccountController : Controller {
+
+    public class AccountController : Controller 
+    {
+        private ManageUser manageUser;
+        public AccountController(ManageUser _manageUser) {
+            manageUser = _manageUser;
+        }
         private string accountSid = Environment.GetEnvironmentVariable("TwilioAcctSid");
         private string authToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
         private string serviceSid = Environment.GetEnvironmentVariable("TwilioServiceSid");
 
         [HttpPost]
-        public ActionResult CreateAccount([FromBody]User user) {
-            var responseMsg = new ResponseMessage(){Status=false,Message="Account already in use"};
-            if (ModelState.IsValid) {
-                if (ManageAccount.AddUser(user)) {
-                    responseMsg.Status = true;
-                    responseMsg.Message = "Successfully created an account";
+        public ActionResult CreateAccount([FromBody]User user) 
+        {
+            var responseMsg = "";
+            Response.StatusCode = 201;
+            if (ModelState.IsValid) 
+            {
+                if (manageUser.AddUser(user)) 
+                {
+                    responseMsg = "Successfully created an account";
+                }
+                else
+                {
+                    Response.StatusCode = 409;
+                    responseMsg = "User account already exists";
                 }
             }
-            else {
-                responseMsg.Message = "Incorrect input. Fix and try again.";
+            else 
+            {
+                Response.StatusCode = 400;
+                responseMsg = "Incorrect input. Fix and try again.";
             }
             return Json(responseMsg);
         }
