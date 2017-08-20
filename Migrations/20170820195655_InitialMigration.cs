0762c81efff00ac2342f2a339165aebcb8787854
cnsheafe <cnsheafe@gmail.com>
2017-08-20 14:15:47 -0600

    Init Migration history for EF Core

---
+++ Migrations/20170820195655_InitialMigration.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace StockWatcher.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StockRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int4", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Equity = table.Column<string>(type: "varchar(4)", maxLength: 4, nullable: false),
+                    Price = table.Column<double>(type: "float8", nullable: false),
+                    RequestId = table.Column<string>(type: "text", nullable: true),
+                    Username = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int4", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false),
+                    Phone = table.Column<string>(type: "varchar(12)", maxLength: 12, nullable: false),
+                    Username = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false),
+                    Uuid = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Phone_Username",
+                table: "Users",
+                columns: new[] { "Phone", "Username" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockRequests");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
