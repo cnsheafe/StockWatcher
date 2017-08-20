ef67f757999494605c5bdeeb170705f2b0a79d95
cnsheafe <cnsheafe@gmail.com>
2017-08-20 14:22:00 -0600

    Added primary keys and table annotations

--- Model/Schemas/Stock.cs
+++ Model/Schemas/Stock.cs
@@ -1,28 +1,35 @@
 // Contains Model Schema for Stock
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StockWatcher.Model.Schemas {
-   public class Stock {
-
-    [Required]
-    [StringLength(30)]
-    public string Username {get; set;}
-
-    [Required]
-    [StringLength(4)]
-    public string Equity {get; set;}
     
-    private double price;
+    [Table("StockRequests")]
+    public class Stock 
+    {
+        [Key]
+        public int Id {get; set;}
 
-    [Required]
-    public double Price {
-        get => price;
-        set => price = Math.Round(value,2,MidpointRounding.ToEven);
+        [Required]
+        [StringLength(30)]
+        public string Username {get; set;}
+
+        [Required]
+        [StringLength(4)]
+        public string Equity {get; set;}
+        
+        private double price;
+
+        [Required]
+        public double Price {
+            get => price;
+            set => price = Math.Round(value,2,MidpointRounding.ToEven);
+        } 
+        public string RequestId {
+            get => Equity + price.ToString();
+            set {}
+        }
+
     } 
-    public string RequestId {
-        get => Equity + price.ToString();
-    }
-
-   } 
 }
--- Model/Schemas/User.cs
+++ Model/Schemas/User.cs
@@ -1,7 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace StockWatcher.Model.Schemas {
-    public class User {
+namespace StockWatcher.Model.Schemas 
+{
+    [Table("Users")]
+    public class User 
+    {
+        [Key]
+        public int Id {get;set;}
+
         [Required]
         [StringLength(30)]
         public string Username {get; set;}
