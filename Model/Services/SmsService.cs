cf2c30d195e1918d8014a11b8595e8ea09c5b4ba
cnsheafe <cnsheafe@gmail.com>
2017-08-21 11:47:25 -0600

    Service to replace SmsAction

---
+++ Model/Services/SmsService.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Twilio;
+using Twilio.Rest.Notify.V1.Service;
+using Twilio.Types;
+
+using StockWatcher.Model.Schemas;
+
+namespace StockWatcher.Model.Services
+{
+    public class SmsService {
+        private string accountSid = 
+            Environment.GetEnvironmentVariable(
+                "TwilioAcctSid"
+            );
+        private string authToken = 
+            Environment.GetEnvironmentVariable(
+                "TwilioAuthToken"
+            );
+        private string serviceSid = 
+            Environment.GetEnvironmentVariable(
+                "TwilioServiceSid"
+            );
+
+        private StockDbContext context;
+        public SmsService(StockDbContext _context) {
+            context = _context;
+        }
+        
+        public NotificationResource NotifyUsers(Stock stock, double openPrice) {
+            // var request = new StockRequestDb(); 
+            // var account = new AccountsDb();
+            // var userIdentities = new List<string>();
+            List<Stock> matchingStocks = context.Stocks
+                .Where(s => s.RequestId == stock.RequestId)
+                .ToList();
+            // foreach (string username in usernames) {
+            //     Console.WriteLine(account.GetUuid(username));
+            //     userIdentities.Add(account.GetUuid(username));
+            // }
+
+            TwilioClient.Init(accountSid, authToken);
+            NotificationResource notification = NotificationResource.Create(
+                serviceSid,
+                identity: userIdentities,
+                body: $"${stock.Equity.ToUpper()} has exceeded target price of {stock.Price} and has reached price ${openPrice}"
+            );
+            return notification;
+        }
+
+
+    }
+}
