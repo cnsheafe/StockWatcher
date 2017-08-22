db6f57b848de3cb5c9575c8b5b81a8e062312759
cnsheafe <cnsheafe@gmail.com>
2017-08-21 20:03:24 -0600

    Changed formatting

--- Model/Services/SmsService.cs
+++ Model/Services/SmsService.cs
@@ -26,18 +26,32 @@ namespace StockWatcher.Model.Services
             );
 
         private StockDbContext context;
-        public SmsService(StockDbContext _context) {
+
+        public SmsService(StockDbContext _context) 
+        {
             context = _context;
         }
-        
+
+        /// <summary>
+        /// Notifies users via SMS about the latest stock price.
+        /// </summary>
+        /// <param name="stock">
+        /// The particular stock whose target price was met.
+        /// </param>
+        /// <param name="openPrice">
+        /// The latest stock price.
+        /// </param>
         public NotificationResource NotifyUsers(Stock stock, double openPrice) 
         {
             var userIdentities = new List<string>();
             List<Stock> matchingStocks = context.Stocks
                 .Where(s => s.RequestId == stock.RequestId)
                 .ToList();
-            foreach (var match in matchingStocks) {
-                var matchingUser = context.Users.Single(u => u.Username == match.Username);
+
+            foreach (var match in matchingStocks) 
+            {
+                var matchingUser = context.Users
+                    .Single(u => u.Username == match.Username);
                 userIdentities.Add(matchingUser.Uuid);
             }
 
@@ -47,6 +61,7 @@ namespace StockWatcher.Model.Services
                 identity: userIdentities,
                 body: $"${stock.Equity.ToUpper()} has exceeded target price of {stock.Price} and has reached price ${openPrice}"
             );
+
             return notification;
         }
 
