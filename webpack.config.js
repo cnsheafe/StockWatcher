1082a56add80a620277703b85e8d257c1239b4cd
cnsheafe <cnsheafe@gmail.com>
2017-08-24 14:44:04 -0600

    Added loader for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,60 @@ const path = require("path");
 module.exports = {
     entry: "./Client/src/index.tsx",
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "./Client")
+        path: path.resolve(__dirname, "Client/dist"),
+        filename: "bundle.js"
     },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { 
-                test: /\.tsx?$/, 
-                loader: "awesome-typescript-loader", 
-                exclude: path.resolve(__dirname,"./node_modules")
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.resolve(__dirname, "Client/src")
+                ],
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        "es2015",
+                        "react"
+                    ]
+                }
             },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { 
-                enforce: "pre", 
-                test: /\.js$/, 
-                loader: "source-map-loader", 
-                exclude: path.resolve(__dirname, "./node_modules")
+            {
+                test: /\.tsx?$/,
+                include: [
+                    path.resolve(__dirname, "Client/src")
+                ],
+                loader: "awesome-typescript-loader",
+                options: {
+                    useBabel: true,
+                    babelOptions: {
+                        presets: [
+                            "es2015",
+                            "react"
+                        ]
+                    },
+                    useCache: true
+                }
             }
         ]
     },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
+    resolve: {
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "Client/src")
+        ],
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
     },
-};
+    devtool: "source-map",
+    devServer: {
+        contentBase: "./Client/dist",
+        compress: true,
+        port: 3000,
+        historyApiFallback: true
+    },
+    context: __dirname,
+    externals: {
+        react: "React",
+        "react-dom": "ReactDOM"
+    }
+}
