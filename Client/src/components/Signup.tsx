1331c6b82c41e203411dec472457edda39585e61
cnsheafe <cnsheafe@gmail.com>
2017-08-24 14:47:04 -0600

    Updated

--- Client/src/components/Signup.tsx
+++ Client/src/components/Signup.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
+import {Link} from "react-router-dom";
 
 export class Signup extends React.Component {
     render() {
-        let signupButton = 
-            <span>
-                <button>Signup</button>
-            </span>;
-        return signupButton;
+        let signupForm: JSX.Element = 
+            <div>
+                <h2>Signup</h2>
+                <form action="" method="post">
+                    <div><input type="text"/></div>
+                    <div><input type="text"/></div> 
+                </form>
+                <Link to="/login">Already have an account? Login!</Link>
+            </div>
+        ;
+        return signupForm;
     }
 }
