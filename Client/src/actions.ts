0623718cd436432a61d9cceae659a46e00bc86ae
cnsheafe <cnsheafe@gmail.com>
2017-08-24 14:45:20 -0600

    Removed interface temporarily

--- Client/src/actions.ts
+++ Client/src/actions.ts
@@ -1,12 +1,8 @@
 export const ADD_GRAPH = "ADD_GRAPH";
 
 
-export interface IAddGraphAction {
-    type: string,
-    equity: string
-}
 
-export function addGraph(equity: string): IAddGraphAction {
+export function addGraph(equity: string) {
     return {
         type: ADD_GRAPH,
         equity
