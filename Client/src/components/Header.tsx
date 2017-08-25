3e417ac987a8ef25e0cd94d6dd8b177994a87ea4
cnsheafe <cnsheafe@gmail.com>
2017-08-25 11:46:03 -0600

    Added login props

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -2,14 +2,28 @@ import * as React from "react";
 
 import {Link} from "react-router-dom";
 
-export class Header extends React.Component {
+export interface ILoginProps {
+    loggedIn: boolean
+}
+
+export class Header extends React.Component<ILoginProps> {
     render() {
-        let header =
-            <div>
-                <h1>StockWatcher</h1>
-                <Link to="/signup">Signup</Link>
-                <Link to="/login">Login</Link>
-            </div>;
+        let header: JSX.Element;
+        if (this.props.loggedIn) {
+            header = 
+                <div>
+                    <h1>StockWatcher</h1>
+                    <button id="account">Account</button>
+                </div>
+        }
+        else {
+            header =
+                <div>
+                    <h1>StockWatcher</h1>
+                    <Link to="/signup">Signup</Link>
+                    <Link to="/login">Login</Link>
+                </div>;
+        }
 
         return header;
     }
