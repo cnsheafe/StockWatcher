e4a74963c6d61b0095a110af062b46496f39a895
cnsheafe <cnsheafe@gmail.com>
2017-08-30 11:53:11 -0600

    changed how html is returned

--- ClientApp/boot-server.tsx
+++ ClientApp/boot-server.tsx
@@ -1,32 +1,31 @@
+import { createServerRenderer, RenderResult } from 'aspnet-prerendering';
 import * as React from 'react';
-// import { Provider } from 'react-redux';
-// import { renderToString } from 'react-dom/server';
-// import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { renderToString } from 'react-dom/server';
+
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {StaticRouter} from "react-router-dom";
+import {createStore} from "redux";
+
+import Index from "./src/Index";
+
 // import { replace } from 'react-router-redux';
 // import { createMemoryHistory } from 'history';
-import { createServerRenderer, RenderResult } from 'aspnet-prerendering';
-// import { routes } from './routes';
-// import configureStore from './configureStore';
 
 export default createServerRenderer(params => {
     return new Promise<RenderResult>((resolve, reject) => {
+
         // Prepare Redux store with in-memory history, and dispatch a navigation event
         // corresponding to the incoming URL
-        // const basename = params.baseUrl.substring(0, params.baseUrl.length - 1); // Remove trailing slash
-        // const urlAfterBasename = params.url.substring(basename.length);
+
         // const store = configureStore(createMemoryHistory());
         // store.dispatch(replace(urlAfterBasename));
 
         // // Prepare an instance of the application and perform an inital render that will
         // // cause any async tasks (e.g., data access) to begin
         // const routerContext: any = {};
-        // const app = (
-        //     <Provider store={ store }>
-        //         <StaticRouter basename={ basename } context={ routerContext } location={ params.location.path } children={ routes } />
-        //     </Provider>
-        // );
-        // renderToString(app);
 
+        const appString = renderToString(Index(true, params));
         // // If there's a redirection, just send this information back to the host application
         // if (routerContext.url) {
         //     resolve({ redirectUrl: routerContext.url });
@@ -37,7 +36,7 @@ export default createServerRenderer(params => {
         // We also send the redux store state, so the client can continue execution where the server left off
         params.domainTasks.then(() => {
             resolve({
-                html: `<h1>Testing message!</h1>`,
+                html: appString,
             });
         }, reject); // Also propagate any errors back into the host application
     });
