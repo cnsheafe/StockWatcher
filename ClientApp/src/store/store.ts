4c104b8626b01a45de7d7f1bd1241e8022a09504
cnsheafe <cnsheafe@gmail.com>
2017-08-31 15:32:53 -0600

    consolidated redux into single file for now

--- ClientApp/src/store/store.ts
+++ ClientApp/src/store/store.ts
@@ -1,7 +1,36 @@
-import { createStore } from "redux";
-import reducer from "./reducer";
+import { createStore, Reducer } from "redux";
 
-const store = createStore(reducer);
+export interface IState {
+    counter: number
+}
 
-export default store;
+const INC = "INCREMENT";
+const DEC = "DECREMENT";
 
+interface Increment { type: "INCREMENT" };
+interface Decrement { type: "DECREMENT" };
+
+type CountAction = Increment | Decrement;
+
+export const actions = {
+    increment: {type: INC} as Increment,
+    decrement: {type: DEC} as Decrement
+};
+
+
+function reducer(state: IState, action: CountAction): IState {
+    switch (action.type) {
+        case INC:
+            return Object.assign({}, state, { counter: state.counter + 1 });
+        case DEC:
+            return Object.assign({}, state, { counter: state.counter -1 });
+        default:
+            return state;
+    }
+}
+
+const initialState: IState = {
+    counter: 0
+};
+
+export default createStore(reducer, initialState);
