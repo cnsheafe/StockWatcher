8bef58101e983df8e438e82d832f005f97672653
cnsheafe <cnsheafe@gmail.com>
2017-08-31 15:35:19 -0600

    added sourcemap plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require("path");
 const appConfig = require("config-tsx");
 const CheckerPlugin = require("awesome-typescript-loader").CheckerPlugin;
 const merge = require("webpack-merge");
+const webpack = require("webpack");
 
 const options = {
-    "client-root": "Client",
+    "client-root": "ClientApp",
     "input-dir": path.posix.normalize(`${__dirname}/ClientApp/`),
     "entry-file": path.posix.normalize(`${__dirname}/ClientApp/boot-server.tsx`),
     "output-dir": path.posix.normalize(`${__dirname}/ClientApp/dist`)
@@ -78,13 +79,20 @@ module.exports = env => {
         devtool: "inline-source-map"
     });
 
+    const clientBundleOutputDir = path.posix.resolve(__dirname, "./wwwroot/dist");
     const clientConfig = merge(sharedConfig(), {
         entry: {
             "client": path.posix.join(appSettings["input-dir"],"boot-client.tsx")
         },
         output: {
-            path: path.posix.resolve(__dirname, "./wwwroot/dist")
-        }
+            path: clientBundleOutputDir
+        },
+        plugins: [
+            new webpack.SourceMapDevToolPlugin({
+                filename: '[file].map', // Remove this line if you prefer inline source maps
+                moduleFilenameTemplate: path.relative(clientBundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
+            })
+        ]
     });
 
     return [clientConfig, serverConfig];
