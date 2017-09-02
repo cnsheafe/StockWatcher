e3cdbc5201f443230ebf0e0bae2852f46f17980a
cnsheafe <cnsheafe@gmail.com>
2017-09-01 18:42:05 -0600

    removes the commas from csv

---
+++ preprocess.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const readline = require("readline");
+
+fs.open("companylist.csv", "r", function(err, inputFd) {
+    if (err) throw err;
+    fs.open("processed-companylist.csv", "w", function(err, outputFd) {
+
+        if (err) throw err;
+        const writeStream = fs.createWriteStream("processed-companylist.csv");
+        const rl = readline.createInterface(
+            fs.createReadStream("companylist.csv"),
+            null
+        );
+
+        rl.on("line", line => {
+            writeStream.write(line.substring(0, line.length -1) +"\n");
+        });
+    });
+});
