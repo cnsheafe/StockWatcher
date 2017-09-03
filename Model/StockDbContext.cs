11210454ebbbb0f89cb8b5d6d03788f681f0e68a
cnsheafe <cnsheafe@gmail.com>
2017-09-02 18:20:03 -0600

    added context for table 'companies'

--- Model/StockDbContext.cs
+++ Model/StockDbContext.cs
@@ -2,9 +2,12 @@ using Microsoft.EntityFrameworkCore;
 
 using StockWatcher.Model.Schemas;
 
-namespace StockWatcher.Model {
-    public class StockDbContext: DbContext {
-        public StockDbContext(DbContextOptions<StockDbContext> options) : base(options) {
+namespace StockWatcher.Model 
+{
+    public class StockDbContext: DbContext 
+    {
+        public StockDbContext(DbContextOptions<StockDbContext> options) : base(options) 
+        {
             var db = base.Database;
             db.OpenConnection();
             db.ExecuteSqlCommand(@"CREATE EXTENSION IF NOT EXISTS ""uuid-ossp"" ");
@@ -13,7 +16,7 @@ namespace StockWatcher.Model {
 
         public DbSet<User> Users {get; set;}
         public DbSet<Stock> Stocks {get; set;}
-
+        public DbSet<Company> Companies {get; set;}
         protected override void OnModelCreating(ModelBuilder modelBuilder) 
         {
             modelBuilder.Entity<User>()
@@ -27,6 +30,16 @@ namespace StockWatcher.Model {
             modelBuilder.Entity<User>()
                 .Property(user => user.Uuid)
                 .HasDefaultValueSql("uuid_generate_v4()");
+
+            modelBuilder.Entity<Company>(entity =>
+            {
+                entity.HasKey(e => e.Symbol);
+                entity.ToTable("companies");
+                entity.Property(e => e.Symbol).ValueGeneratedNever();
+                entity.Property(e => e.Name).HasColumnName("Name");
+                entity.Property(e => e.Adrtso).HasColumnName("ADRTSO");
+                entity.Property(e => e.IPOyear).HasColumnName("IPOyear");
+            });
         }
     }
 }
