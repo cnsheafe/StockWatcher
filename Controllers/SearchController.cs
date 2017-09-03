97795a71b84c5644eff7512b88327732a0291c4d
cnsheafe <cnsheafe@gmail.com>
2017-09-02 18:19:20 -0600

    Endpoint for retrieving companies

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+
+using Newtonsoft;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using StockWatcher.Model.Schemas;
+using StockWatcher.Model.Services;
+
+namespace StockWatcher.Controllers
+{
+    public class SearchController: Controller
+    {
+        private readonly QueryCompanyService service;
+        public SearchController(QueryCompanyService _service)
+        {
+            service = _service;
+        }
+
+        [HttpGet("/company")]
+        public IActionResult GetCompany()
+        {
+            var queryTerms = Request.Query;
+            Query query = new Query();
+
+            IEnumerable<PropertyInfo> allowedKeys = typeof(Query).GetProperties();
+
+            foreach (var item in allowedKeys)
+            {
+                var key = item.Name.ToString();
+                if (!queryTerms.ContainsKey(key))
+                {
+                    Response.StatusCode = 400;
+                    return Json("Error");
+                }
+            }    
+
+            StringValues tmp;
+            queryTerms.TryGetValue("searchphrase", out tmp);
+            query.SearchPhrase = tmp;
+            queryTerms.TryGetValue("isequity", out tmp);
+            query.IsEquity = tmp;
+            Response.StatusCode = 200;
+            JsonResult apiResult = Json(service.SearchCompanies(query));
+
+            return apiResult;
+        }
+    }
+}
