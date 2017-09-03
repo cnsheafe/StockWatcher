fc2664230a27b2ea86d23f38ed157fd9157f54b6
cnsheafe <cnsheafe@gmail.com>
2017-09-02 18:16:16 -0600

    model for table 'companies'

---
+++ Model/Schemas/Company.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockWatcher.Model.Schemas
+{
+    [Table("companies")]
+    public class Company 
+    {
+        [Key]
+        public string Symbol {get; set;}
+        public string Name {get; set;}
+        public string LastSale {get; set;}
+        public string MarketCap {get; set;}
+        public string Adrtso {get; set;}
+
+        public string IPOyear {get; set;}
+
+        public string Sector {get; set;}
+
+        public string Industry {get; set;}
+        public string SummaryQuote {get; set;}
+    }
+}
