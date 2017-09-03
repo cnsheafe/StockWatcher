4658229b88fd1baa28d15e8d49da958cfb193070
cnsheafe <cnsheafe@gmail.com>
2017-09-02 18:18:55 -0600

    query companies

---
+++ Model/Schemas/Query.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockWatcher.Model.Schemas
+{
+    public class Query
+    {
+        [Required]
+        public string SearchPhrase {get; set;}
+
+        [Required]
+        public string IsEquity {get; set;}
+
+    }
+}
