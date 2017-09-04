428e9822692b9aca9cf2df7442c2a4f761b18e60
cnsheafe <cnsheafe@gmail.com>
2017-09-03 21:28:56 -0600

    added observable to component for querying

--- ClientApp/src/components/Search.tsx
+++ ClientApp/src/components/Search.tsx
@@ -1,7 +1,35 @@
 import * as React from "react";
+import * as Rx from "rxjs";
 
-export class Search extends React.Component {
+export class Search extends React.Component<{}, {}> {
+
     render() {
-        return <input type="text"/>;
+        return (
+            <input id="search-companies" type="text"/>
+        );
     }
+
+    componentDidMount() {
+        const searchElement = document.getElementById("search-companies") as HTMLInputElement;
+
+        Rx.Observable.fromEvent(searchElement,"keyup")
+            .debounceTime(300)
+            .subscribe(() => fetchCompanies(
+                searchElement.value,
+                true
+        ));
+    }
+    
+}
+
+function fetchCompanies(searchPhrase: string, isSymbol: boolean):Promise<JSON> {
+    let searchRequest = new Request(`/company/?searchphrase=${searchPhrase}&issymbol=${isSymbol.toString()}`, {
+        method: "GET"
+    });
+
+    return fetch(searchRequest)
+        .then(res => {
+            console.log(res.json());
+            return res.json();
+        });
 }
