3996342c014f987e838c929fd78d38d71755b9dd
cnsheafe <cnsheafe@gmail.com>
2017-09-08 11:23:39 -0600

    using a temporary key for auth - CHANGE TO ENV

--- Model/Services/JWTService.cs
+++ Model/Services/JWTService.cs
@@ -19,16 +19,19 @@ namespace StockWatcher.Model.Services
                 {"iat", DateTime.Now}
             };
             // TODO: Add env variables
-            RSA privateKey;
 
-            byte[] cipher = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("cipher"));
-            string passphrase = Environment.GetEnvironmentVariable("passphrase");
-            using (var certificate = new X509Certificate2(cipher, passphrase))
-            {
-                privateKey = certificate.GetRSAPrivateKey();
-            }
+            // byte[] cipher = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("cipher"));
+            // string passphrase = Environment.GetEnvironmentVariable("passphrase");
+            byte[] secret = new byte[]{146, 187, 63, 46, 207, 128, 188, 94, 86, 91, 7, 79, 17, 80, 253, 228, 119, 218, 148, 89, 39, 129, 137, 217, 38, 31, 130, 227, 191, 234, 58, 175};
+            // string passphrase = "mypass";
+            // byte[] privateKey;
+            // using (var rsa = new RSACryptoServiceProvider(256))
+            // {
+            //     privateKey = rsa.Encrypt(cipher, true);
 
-            return JWT.Encode(payload, privateKey, JwsAlgorithm.RS256);
+            // }
+
+            return JWT.Encode(payload, secret, JwsAlgorithm.HS256);
         }
     }
 }
