d32734d912146906f671f8500909265f7aa99927
cnsheafe <cnsheafe@gmail.com>
2017-09-07 20:57:36 -0600

    added labels to chart

--- ClientApp/src/components/Graphs.tsx
+++ ClientApp/src/components/Graphs.tsx
@@ -3,58 +3,59 @@ import * as Chart from "chart.js";
 import { connect } from "react-redux";
 
 import { IState } from "../store/store";
-import { Graph } from "../store/data-blocks";
+import { Graph, DataPoints } from "../store/data-blocks";
 
 
 interface GraphsProps {
-    graphs: Array<Graph>
+  graphs: Array<Graph>
 }
 
 class Graphs extends React.Component<GraphsProps, {}> {
-    render() {
-        const graphs = this.props.graphs.map<JSX.Element>((graph: Graph,index: number) => {
-            return (
-                <li key={index}><canvas id={graph.graphId}></canvas></li>
-            );
-        });
+  render() {
+    const graphs = this.props.graphs.map<JSX.Element>((graph: Graph, index: number) => {
+      return (
+        <li key={index}><canvas id={graph.graphId}></canvas></li>
+      );
+    });
 
-        return (
-            <ul id="graphs">
-                {graphs}
-            </ul>
-        );
-    }
+    return (
+      <ul id="graphs">
+        {graphs}
+      </ul>
+    );
+  }
 
-    componentDidUpdate() {
-        this.props.graphs.forEach(graph => {
-            let context = document.getElementById(graph.graphId) as HTMLCanvasElement;
-            let config: Chart.ChartConfiguration = ChartConfigurationBuilder<number>("line", graph.dataset);
+  componentDidUpdate() {
+    this.props.graphs.forEach(graph => {
+      let context = document.getElementById(graph.graphId) as HTMLCanvasElement;
+      let config: Chart.ChartConfiguration = ChartConfigurationBuilder("line", graph.dataset, graph.labels);
 
-            let chart = new Chart(context, config);
-        })
-    }
+      let chart = new Chart(context, config);
+    })
+  }
 }
 
-function ChartConfigurationBuilder<T>(type: Chart.ChartType, dataPoints: Array<T>): Chart.ChartConfiguration {
+function ChartConfigurationBuilder(type: Chart.ChartType, dataPoints: Array<number>, labels: Array<string>): Chart.ChartConfiguration {
 
-    let chartDataSets: Chart.ChartDataSets[] = [{
-        data: dataPoints
-    }];
+  let chartDataSets: Chart.ChartDataSets[] = [{
+    data: dataPoints
+  }];
 
-    let chartData: Chart.ChartData = {
-        datasets: chartDataSets
-    };
+  let chartData: Chart.ChartData = {
+    datasets: chartDataSets,
+    labels: labels
+  };
 
-    return {
-        type: type,
-        data: chartData
-    };
+  return {
+    type: type,
+    data: chartData
+  };
 }
 
 function mapStateToProps(state: IState) {
-    return {
-        graphs: state.graphs
-    }
+  return {
+    graphs: state.graphs
+  }
 }
 
 export default connect(mapStateToProps)(Graphs);
