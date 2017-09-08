2be5a59239523f4fe126b511f5071b87aec9360c
cnsheafe <cnsheafe@gmail.com>
2017-09-07 20:58:39 -0600

    added async action

--- ClientApp/src/store/actions.ts
+++ ClientApp/src/store/actions.ts
@@ -2,49 +2,75 @@ import { ActionCreator, Dispatch } from "redux";
 import thunk, { ThunkAction } from "redux-thunk";
 
 import { Company, Graph } from "./data-blocks";
-
+import { IState } from "./store";
 
 // Action Commands
 export const SEARCH = "SEARCH_RESULT";
 export const RES_INDEX = "SEARCH_RESULT_INDEX";
 export const LOGIN = "LOGGED_IN";
-
+export const GRAPH = "ADD_GRAPH";
 // Action Interfaces
 export interface LoginAction { type: "LOGGED_IN" }
 
 export interface SearchResult {
-    type: "SEARCH_RESULT",
-    results: Array<Company>
+  type: "SEARCH_RESULT",
+  results: Array<Company>
 }
 
 export interface CompanyFromIndex {
-    type: "SEARCH_RESULT_INDEX",
-    index: number
+  type: "SEARCH_RESULT_INDEX",
+  index: number
 }
 
-export type ValidAction = SearchResult & LoginAction & CompanyFromIndex;
-
-export const ListSearchResults: ActionCreator<SearchResult> = 
-    (results: Array<Company>) => {
-        return {
-            type: SEARCH,
-            results: results
-        }
-    }
-
-export const QueryCompanyStock: ActionCreator<CompanyFromIndex> = 
-    (index: number) => {
-        return {
-            type: RES_INDEX,
-            index: index
-        }
-    }
-
-// export const GetCompanyAsync: ThunkAction =
-//     ()
-export function GetCompanyAsync () {
-    return function (dispatch) {
-        let url = "";
-        return fetch(url)
-    }
+export interface AddGraph {
+  type: "ADD_GRAPH",
+  company: Company,
+  dataPoints: Array<number>,
+  labels: Array<string>
 }
+
+export type ValidAction = SearchResult & LoginAction & CompanyFromIndex & AddGraph;
+
+// Action for updating state with list of matching company names from database
+// Used on Search.tsx
+export const ListSearchResults: ActionCreator<SearchResult> =
+  (results: Array<Company>) => {
+    return {
+      type: SEARCH,
+      results: results
+    }
+  }
+// Action for Adding a Graph
+
+export const addGraphAsync = 
+  (company: Company) => {
+
+    return function(dispatch: Dispatch<IState>) {
+      let stockRequest = new Request(
+        `/stockprice?stocksymbol=${company.symbol}`,
+        { method: "GET" });
+      
+      return fetch(stockRequest)
+        .then(res => {
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json);
+          let dataPoints = [];
+          let labels = [];
+
+          for (var i = json.length - 1; i >= 0 ; i--) {
+            dataPoints.push(json[i].price);
+            labels.push(json[i].timeStamp);
+          }
+
+          dispatch<AddGraph>({
+            type: GRAPH,
+            company: company,
+            dataPoints: dataPoints,
+            labels: labels
+          });
+        })
+    }
+  }
+
