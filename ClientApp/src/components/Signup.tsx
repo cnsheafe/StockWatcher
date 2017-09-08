3d276a420b88cd2bb2a049d4d26ed14eef8e10cc
cnsheafe <cnsheafe@gmail.com>
2017-09-07 20:58:10 -0600

    added fields

--- ClientApp/src/components/Signup.tsx
+++ ClientApp/src/components/Signup.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export class Signup extends React.Component {
-    render() {
-        let signupForm: JSX.Element = 
-            <div>
-                <h2>Signup</h2>
-                <form action="" method="post">
-                    <div><input type="text"/></div>
-                    <div><input type="text"/></div> 
-                </form>
-                <Link to="/login">Already have an account? Login!</Link>
-            </div>
-        ;
-        return signupForm;
-    }
+  render() {
+    let signupForm: JSX.Element =
+      <div>
+        <h2>Signup</h2>
+        <form action="/user/createuser" method="post" encType="application/json">
+          <div>Username<input type="text"name="Username" required={true}/></div>
+          <div>Password<input type="password" name="Password" required={true}/></div>
+          <div>Phone Number<input type="tel" name="Phone" required={true}/></div>
+          <div><button type="submit">Signup</button></div>
+        </form>
+        <Link to="/login">Already have an account? Login!</Link>
+      </div>
+      ;
+    return signupForm;
+  }
 }
