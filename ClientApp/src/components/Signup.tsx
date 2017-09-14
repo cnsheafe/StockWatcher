c13aaab8e3c04ec8f0be9505d3e26cd997b57465
cnsheafe <cnsheafe@gmail.com>
2017-09-14 15:10:14 -0600

    commit for future account support
    This commit is only for file reference
    for when account functionality is
    re-assessed in the future.

--- ClientApp/src/components/Signup.tsx
+++ ClientApp/src/components/Signup.tsx
@@ -27,15 +27,43 @@ export class Signup extends React.Component {
 
   render() {
     return(
-      <div>
-        <h2>Signup</h2>
-        <form id="signup-form" onSubmit={event => this.sendSignupRequest(event)}>
-          <div>Username<input type="text"name="Username" required={true}/></div>
-          <div>Password<input type="password" name="Password" required={true}/></div>
-          <div>Phone Number<input type="tel" name="Phone" required={true}/></div>
-          <div><button type="submit">Signup</button></div>
+      <div className="signup">
+        <h2 className="signup-title">Signup</h2>
+        <form 
+          id="signup-form" 
+          onSubmit={event => this.sendSignupRequest(event)}
+          className="signup-form">
+          <div className="signup-username">
+            <label htmlFor="signup-input-username">Username</label>
+            <input 
+              type="text" 
+              name="Username" 
+              id="signup-input-username" 
+              required={true}/>
+          </div>
+          <div className="signup-password">
+            <label htmlFor="signup-input-password">Password</label>
+            <br/>
+            <input 
+              type="password" 
+              name="Password" 
+              id="signup-input-password"
+              required={true}/>
+          </div>
+          <div className="signup-phone">
+            <label htmlFor="signup-input-phone">Phone Number</label>
+            <br/>
+            <input 
+              type="tel" 
+              name="Phone" 
+              id="signup-input-phone"
+              required={true}/>
+          </div>
+          <div className="signup-submit">
+            <button type="submit">Signup</button>
+          </div>
         </form>
-        <Link to="/login">Already have an account? Login!</Link>
+        <Link to="/login" className="goto-login">Already have an account? Login!</Link>
       </div>
     );
   }
