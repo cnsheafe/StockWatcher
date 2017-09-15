ecc35da550857302359ebd25658d87d71bdc8d5b
cnsheafe <cnsheafe@gmail.com>
2017-09-15 11:44:02 -0600

    updated ef core migrations

--- Migrations/20170915164655_Requests.Designer.cs
+++ Migrations/20170915164655_Requests.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace StockWatcher.Migrations
 {
     [DbContext(typeof(StockDbContext))]
-    partial class StockDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170915164655_Requests")]
+    partial class Requests
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20170915164655_Requests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace StockWatcher.Migrations
+{
+    public partial class Requests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockRequests");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "companies",
+                columns: table => new
+                {
+                    Symbol = table.Column<string>(type: "text", nullable: false),
+                    ADRTSO = table.Column<string>(type: "text", nullable: true),
+                    IPOyear = table.Column<string>(type: "text", nullable: true),
+                    Industry = table.Column<string>(type: "text", nullable: true),
+                    LastSale = table.Column<string>(type: "text", nullable: true),
+                    MarketCap = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Sector = table.Column<string>(type: "text", nullable: true),
+                    SummaryQuote = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_companies", x => x.Symbol);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int4", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Price = table.Column<double>(type: "float8", nullable: false),
+                    RequestId = table.Column<string>(type: "text", nullable: false),
+                    TwilioBinding = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "companies");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.CreateTable(
+                name: "StockRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Equity = table.Column<string>(maxLength: 4, nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    RequestId = table.Column<string>(nullable: true),
+                    Username = table.Column<string>(maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Email = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(maxLength: 30, nullable: false),
+                    Phone = table.Column<string>(maxLength: 12, nullable: false),
+                    Username = table.Column<string>(maxLength: 30, nullable: false),
+                    Uuid = table.Column<string>(nullable: true, defaultValueSql: "uuid_generate_v4()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Phone_Username",
+                table: "Users",
+                columns: new[] { "Phone", "Username" },
+                unique: true);
+        }
+    }
+}
