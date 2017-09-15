9b3f07c7c0bccf5975d7b0a3fe1decb34c58851d
cnsheafe <cnsheafe@gmail.com>
2017-09-15 13:25:42 -0500

    attached app to the store

--- ClientApp/src/components/App.tsx
+++ ClientApp/src/components/App.tsx
@@ -1,14 +1,30 @@
 import * as React from "react";
 import Header from "./Header";
 import { Body } from "./Body";
+import {connect} from "react-redux";
+import store, {IState} from "../store/store";
+import { toggleModalDisplay } from "../store/actions";
 
-export class App extends React.Component {
+interface AppProps {
+  showModal: boolean
+}
+
+
+class App extends React.Component<AppProps, {}> {
   render() {
     return (
-      <main>
+      <main className={this.props.showModal ? "DimBody" : ""}>
         <Header />
         <Body />
       </main>
     );
   }
 }
+
+function mapStateToProps(state: IState) {
+  return {
+    showModal: state.showModal
+  }
+}
+
+export default connect(mapStateToProps)(App);
