b8edf6a90a3cd7ec146f1a5990bc93945e81c220
cnsheafe <cnsheafe@gmail.com>
2017-09-17 23:34:05 -0600

    added a connection string parser and split code based on environment(e.g. development)

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,8 @@ using Hangfire;
 using Hangfire.AspNetCore;
 using Hangfire.PostgreSql;
 
+using Npgsql;
+
 using StockWatcher.Model;
 using StockWatcher.Model.Services;
 
@@ -22,36 +24,81 @@ namespace StockWatcher
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        /// <summary>
+        /// Parses connection string of the form "postgres://username:password@host:portNumber/database"
+        /// into connection string of form "Username=username;Password=password;Host=host;Port=portNumber;Database=database;"
+        /// </summary>
+        /// <param name="rawConnectionString">
+        /// Connection string of the form "postgres://username:password@host:portNumber/database"
+        /// </param>
+        /// <returns>
+        /// Connection string of the form "Username=username;Password=password;Host=host;Port=portNumber;Database=database;"
+        /// </returns>
+        private string ConnectionStringParser(string rawConnectionString)
         {
-            Configuration = configuration;
+            int firstIndex = rawConnectionString.IndexOf("//");
+            int lastIndex = rawConnectionString.IndexOf("@");
 
-            // var builder = new ConfigurationBuilder()
-            //     .SetBasePath(env.ContentRootPath)
-            //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            //     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-            //     .AddEnvironmentVariables();
-            // Configuration = builder.Build();
+            string usernamePassword = rawConnectionString.Substring(firstIndex + 2, lastIndex - firstIndex - 2);
+            string[] tmp = usernamePassword.Split(":");
+            string username = tmp[0];
+            string password = tmp[1];
 
+            firstIndex = lastIndex + 1;
+            lastIndex = rawConnectionString.LastIndexOf("/");
+
+            string hostPort = rawConnectionString.Substring(firstIndex, lastIndex - firstIndex);
+
+            tmp = hostPort.Split(":");
+            string host = tmp[0];
+            int port = int.Parse(tmp[1]);
+
+            string database = rawConnectionString.Substring(lastIndex + 1);
+            
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder();
+            connectionStringBuilder.Username = username;
+            connectionStringBuilder.Password = password;
+            connectionStringBuilder.Host = host;
+            connectionStringBuilder.Port = port;
+            connectionStringBuilder.Database = database;
+            connectionStringBuilder.SslMode = SslMode.Prefer;
+            connectionStringBuilder.TrustServerCertificate = true;
+
+            return connectionStringBuilder.ConnectionString;
         }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        private void ServicesHelper(IServiceCollection services, string connectionString) 
         {
             services.AddMvc();
             services.AddHangfire(config =>
-                config.UsePostgreSqlStorage(Configuration.GetConnectionString("HangfireConnection"))
+                config.UsePostgreSqlStorage(connectionString)
             );
 
             services.AddDbContext<StockDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("PostGresConnection"))
+                options.UseNpgsql(connectionString)
             );
 
             services.AddTransient<StockRequestService>();
             services.AddTransient<QueryCompanyService>();
             services.AddTransient<AlphaVantageService>();
+        }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services) 
+        {
+            string connectionString = Configuration.GetConnectionString("PostGresConnection");
+            ServicesHelper(services, connectionString);
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connectionString = ConnectionStringParser(Environment.GetEnvironmentVariable("DATABASE_URL"));
+            ServicesHelper(services, connectionString);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -87,8 +134,12 @@ namespace StockWatcher
                 //     defaults: new { controller = "Home", action = "Index" });
             });
 
-            app.UseHangfireDashboard();
-            app.UseHangfireServer();
+            // app.UseHangfireDashboard();
+            
+            app.UseHangfireServer(
+                // Heroku Postgres has max connections of 20
+                new BackgroundJobServerOptions{ WorkerCount = 5}
+            );
         }
     }
 }
