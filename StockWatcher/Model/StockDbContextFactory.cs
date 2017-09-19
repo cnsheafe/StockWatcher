9fa8988f19f47a9bd0a83e2b9e3aace7bf22ba46
cnsheafe <cnsheafe@gmail.com>
2017-09-18 22:08:40 -0600

    Changed env variable name and using helper class

--- StockWatcher/Model/StockDbContextFactory.cs
+++ StockWatcher/Model/StockDbContextFactory.cs
@@ -3,15 +3,22 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
+using Npgsql;
+
+using StockWatcher.Model.Services.Helpers;
+
 namespace StockWatcher.Model
 {
     public class StockDbContextFactory : IDesignTimeDbContextFactory<StockDbContext>
     {
+
         public StockDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<StockDbContext>();
-            Console.WriteLine($"connstring {Environment.GetEnvironmentVariable("SWConnString")}");
-            optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("SWConnString"));
+
+            optionsBuilder.UseNpgsql(
+                ConnectionStringParser.Parse(args[0])
+            );
             return new StockDbContext(optionsBuilder.Options);
         }
     }
